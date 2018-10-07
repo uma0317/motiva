373ade61deec1cd05ad58620d901d46e41f3c1d7
uma0317 <blog3170@gmail.com>
2018-10-07 12:07:35 +0900

    first commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,41 @@
+<template>
+	<div>
+		<line-chart></line-chart>
+	</div>
+</template>
+
+<script>
+import LineChart from '@/components/LineChart'
+export default {
+	name: 'hello',
+	components: {
+		LineChart
+	},
+	data () {
+		return {
+			msg: 'Welcome to Your Vue.js PWA'
+		}
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #35495E;
+}
+</style>
