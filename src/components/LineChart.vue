30c61fad713bc8b470029aac429b1288fcaf2713
uma0317 <blog3170@gmail.com>
2018-10-07 12:49:22 +0900

    x軸ラベル削除

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -8,41 +8,61 @@
       return {
         datacollection: {
         //Data to be represented on x-axis
-          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'], 
+          labels: ['好きな人ができた', '学芸会で主役', '先生に怒られた', '主役', '指揮者をやった', 'バドの全道大会', '高校受験合格', '大学落ちた', '大学受かった', 'ドキュメント見た', 'プログラミングスタート', '逆求人で色々なインターン参加'], 
           datasets: [
             {
               label: 'Data One',
-              backgroundColor: '#f87979',
+              borderColor: '#f87979',
               pointBackgroundColor: 'white',
               borderWidth: 1,
               pointBorderColor: '#249EBF',
               //Data to be represented on y-axis
-              data: [40, 20, 30, 30, 50, 50, 90, 10, 20, 40, 50, 70, 90, 100]
+              data: [40, 60, -50, 70, 70, 50, 80, -80, 80, 70, 90, 100],
+              fill: false
             }
           ]
         },
         //Chart.js options that controls the appearance of the chart
+       
         options: {
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero: true
-              },
-              gridLines: {
-                display: true
-              }
-            }],
-            xAxes: [ {
-              gridLines: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        suggestedMin: -100
+                    },
+                    gridLines: {
+                        display: true
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Motivation',
+                        fontFamily: 'monospace',
+                        fontSize: 14
+                    },
+                }],
+                xAxes: [ {
+                    display: false,
+                    gridLines: {
+                        display: false
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: '時間軸',
+                        fontFamily: 'monospace',
+                        fontSize: 14
+                    },
+                }]
+            },
+            legend: {
                 display: false
-              }
-            }]
-          },
-          legend: {
-            display: true
-          },
-          responsive: true,
-          maintainAspectRatio: false
+            },
+            title: {
+                display: false
+            },
+            
+            responsive: true,
+            maintainAspectRatio: false
         }
       }
     },
