610372056ac33e5bb565cde77b6bb0d55b19f42a
uma0317 <blog3170@gmail.com>
2018-10-10 01:28:04 +0900

    complete

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -1,74 +1,117 @@
 <script>
-  //Importing Line class from the vue-chartjs wrapper
-  import {Line} from 'vue-chartjs'
-  //Exporting this so it can be used in other components
-  export default { 
-    extends: Line,
-    data () {
-      return {
-        datacollection: {
-        //Data to be represented on x-axis
-          labels: ['好きな人ができた', '学芸会で主役', '先生に怒られた', '主役', '指揮者をやった', 'バドの全道大会', '高校受験合格', '大学落ちた', '大学受かった', 'ドキュメント見た', 'プログラミングスタート', '逆求人で色々なインターン参加'], 
-          datasets: [
-            {
-              label: 'Data One',
-              borderColor: '#f87979',
-              pointBackgroundColor: 'white',
-              borderWidth: 1,
-              pointBorderColor: '#249EBF',
-              //Data to be represented on y-axis
-              data: [40, 60, -50, 70, 70, 50, 80, -80, 80, 70, 90, 100],
-              fill: false
-            }
-          ]
-        },
-        //Chart.js options that controls the appearance of the chart
-       
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true,
-                        suggestedMin: -100
+    import { Line, mixins } from 'vue-chartjs'
+
+    import store from '@/store/index'
+    import { mapState, mapGetters } from 'vuex'
+    export default {
+        extends: Line,
+        data () {
+            return {
+                //Chart.js options that controls the appearance of the chart
+            
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true,
+                                suggestedMin: -100,
+                                suggestedMax: 100,
+                            },
+                            gridLines: {
+                                display: true
+                            }
+                        }],
+                        xAxes: [ {
+                            display: false,
+                            gridLines: {
+                                display: false
+                            },
+                            scaleLabel: {
+                                display: true,
+                                labelString: '時間軸',
+                                fontFamily: 'monospace',
+                                fontSize: 14
+                            },
+                        }]
                     },
-                    gridLines: {
-                        display: true
-                    },
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Motivation',
-                        fontFamily: 'monospace',
-                        fontSize: 14
-                    },
-                }],
-                xAxes: [ {
-                    display: false,
-                    gridLines: {
+                    legend: {
                         display: false
                     },
-                    scaleLabel: {
-                        display: true,
-                        labelString: '時間軸',
-                        fontFamily: 'monospace',
-                        fontSize: 14
+                    title: {
+                        display: false
                     },
-                }]
+                    tooltips: {
+                        mode: 'nearest'
+                    },
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    events: ['click', 'mousemove'],
+                    onClick: function (e, el) {
+                        if (! el || el.length === 0) return;
+                        store.commit("setCurrentIndex", el[0]._index)
+                        console.log(el[0]);
+                    }
+                }
+            }
+        },
+        computed: {
+            ...mapGetters([
+                'getEvents',
+            ]),
+            ...mapState({
+                events: state => state.events
+            }),
+
+            getLabels() {
+                return this.events.map(event => { return event.name })
             },
-            legend: {
-                display: false
+
+            getValues() {
+                return this.events.map(event => { return event.value })
             },
-            title: {
-                display: false
-            },
-            
-            responsive: true,
-            maintainAspectRatio: false
+
+            datacollection() {
+                return {
+                    labels: this.getLabels,
+                    datasets: [
+                        {
+                            label: 'Event',
+                            borderColor: '#f87979',
+                            pointBackgroundColor: 'white',
+                            borderWidth: 1,
+                            pointBorderColor: '#249EBF',
+                            //Data to be represented on y-axis
+                            data: this.getValues,
+                            fill: false,
+                            pointRadius: 8,
+                            pointHoverRadius: 8
+                        }
+                    ]
+                }
+            }
+        },
+
+        methods: {
+            getClickedEvents(evt) {
+                var activePoints = this.getElementsAtEvent(evt);
+                console.log(activePoints)
+            }
+        },
+
+        watch: {
+            events (newCevents, oldCevents) {
+                console.log("change")
+                this.renderChart(this.datacollection, this.options)                
+            }
+        },
+
+        created () {
+            store.commit('fetchEvents')
+        },
+
+        mounted () {
+        //renderChart function renders the chart with the datacollection and options object.
+            this.renderChart(this.datacollection, this.options)
         }
-      }
-    },
-    mounted () {
-    //renderChart function renders the chart with the datacollection and options object.
-      this.renderChart(this.datacollection, this.options)
     }
-  }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,12 +3,22 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuex from 'vuex'
+import store from '@/store/index'
+import VueMaterial from 'vue-material'
+import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+Vue.use(Vuex)
+Vue.use(VueMaterial)
+Vue.use(MdButton)
+
 new Vue({
   el: '#app',
+  store,
   router,
   template: '<App/>',
   components: { App }
